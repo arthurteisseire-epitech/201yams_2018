a8aaa6e20ef67b5eea72f60984e93e09b3a7ae4c
arthur <arthur.teisseire@epitech.eu>
2019-02-14 14:26:46 +0100

    Check same value for full

--- proba.py
+++ proba.py
@@ -7,6 +7,9 @@ class Op:
 
     @staticmethod
     def full(dices, x, y):
+        if x == y:
+            print("for full dices values mustn't be the sames")
+            exit(84)
         print("full:")
         print(dices, x, y)
 
