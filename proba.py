bb8d3ac9a9535b0a4009b8c0f9b273ec10e9a612
arthur <arthur.teisseire@epitech.eu>
2019-02-14 18:04:58 +0100

    Add four three and pair : pair isn't working (round)

--- proba.py
+++ proba.py
@@ -1,17 +1,30 @@
 import numpy as np
 import math
 
+_round = 2
+
 
 class Op:
 
     @staticmethod
     def yams(dices, x):
-        return lb(len(dices), 5)
+        return round(lb(len(dices), 5), _round)
+
+    @staticmethod
+    def four(dices, x):
+        return round(lb(len(dices), 4) + Op.yams(dices, x), _round)
+
+    @staticmethod
+    def three(dices, x):
+        return round(lb(len(dices), 3) + Op.four(dices, x), _round)
+
+    @staticmethod
+    def pair(dices, x):
+        return round(lb(len(dices), 2) + Op.three(dices, x), _round)
 
 
 def lb(n, k):
     proba_ok = pow(1.0 / 6.0, k)
     proba_ko = pow(5.0 / 6.0, n - k)
-    comb = math.factorial(n) / math.factorial(k) * math.factorial(n - k)
-    return round(proba_ok * proba_ko * comb * 100, 2)
-
+    comb = math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+    return proba_ok * proba_ko * comb * 100
--- test_op.py
+++ test_op.py
@@ -3,8 +3,15 @@ from proba import Op
 
 
 class TestOp(TestCase):
-    def test_pair(self):
+    def test_yams(self):
         self.assertEqual(Op.yams([0, 0, 0, 0, 0], 4), 0.01)
+        # self.assertEqual(Op.yams([4, 1, 1, 1, 1], 4), Op.four([0, 0, 0, 0, 0], 4))
 
     def test_four(self):
-        self.assertEqual(Op.four([0, 0, 0, 0, 0], 4), 1.86)
+        self.assertEqual(Op.four([0, 0, 0, 0, 0], 4), 0.33)
+
+    def test_three(self):
+        self.assertEqual(Op.three([0, 0, 0, 0, 0], 4), 3.55)
+
+    # def test_pair(self):
+    #     self.assertEqual(Op.pair([0, 0, 0, 0, 0], 4), 19.62)
