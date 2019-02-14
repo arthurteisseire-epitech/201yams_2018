a2e1da19024de0803a67ec3d07919b276386ee7d
arthur <arthur.teisseire@epitech.eu>
2019-02-14 12:44:00 +0100

    Add template function + parser

--- parse.py
+++ parse.py
@@ -1,13 +1,26 @@
 import argparse
+from proba import *
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("d1", type=int, help="value of the first die (0 if not thrown)")
-    parser.add_argument("d2", type=int, help="value of the second die (0 if not thrown)")
-    parser.add_argument("d3", type=int, help="value of the third die (0 if not thrown)")
-    parser.add_argument("d4", type=int, help="value of the fourth die (0 if not thrown)")
-    parser.add_argument("d5", type=int, help="value of the fifth die (0 if not thrown)")
-    parser.add_argument("c", type=str, help="expected combination")
-    args = parser.parse_args()
-    return args
+class Parser:
+
+    @staticmethod
+    def parse_args():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("d1", type=int, help="value of the first die (0 if not thrown)")
+        parser.add_argument("d2", type=int, help="value of the second die (0 if not thrown)")
+        parser.add_argument("d3", type=int, help="value of the third die (0 if not thrown)")
+        parser.add_argument("d4", type=int, help="value of the fourth die (0 if not thrown)")
+        parser.add_argument("d5", type=int, help="value of the fifth die (0 if not thrown)")
+        parser.add_argument("c", type=str, help="expected combination")
+        args = parser.parse_args()
+        return args
+
+    @staticmethod
+    def exec_func():
+        args = Parser.parse_args()
+        dices = [args.d1, args.d2, args.d3, args.d4, args.d5]
+
+        array = str(args.c).split("_")
+        func_name = array[0]
+        exec("Op." + func_name + "(dices," + ",".join(array[1:]) + ")")
---
+++ proba.py
@@ -0,0 +1,13 @@
+class Op:
+
+    @staticmethod
+    def pair(x):
+        print("pair:")
+        print(x)
+
+    @staticmethod
+    def full(dices, x, y):
+        print("full:")
+        print(dices)
+        print(x)
+        print(y)
