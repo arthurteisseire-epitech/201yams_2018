71f6dffaf2f0b8b04339dc353a40fe57cb81328a
arthur <arthur.teisseire@epitech.eu>
2019-02-14 18:18:32 +0100

    Remove useless comment

--- test_op.py
+++ test_op.py
@@ -5,7 +5,6 @@ from proba import Op
 class TestOp(TestCase):
     def test_yams(self):
         self.assertEqual(round(Op.yams([0, 0, 0, 0, 0], 4), 2), 0.01)
-        # self.assertEqual(Op.yams([4, 1, 1, 1, 1], 4), Op.four([0, 0, 0, 0, 0], 4))
 
     def test_four(self):
         self.assertEqual(round(Op.four([0, 0, 0, 0, 0], 4), 2), 0.33)
