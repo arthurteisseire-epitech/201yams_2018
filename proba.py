fbacbd6f68993f55502eecde037f46abfccbeebc
arthur <arthur.teisseire@epitech.eu>
2019-02-15 13:20:09 +0100

    Make binomial law rescursive

--- proba.py
+++ proba.py
@@ -9,19 +9,21 @@ class Op:
 
     @staticmethod
     def four(dices, x):
-        return lb(len(dices), 4) + Op.yams(dices, x)
+        return lb(len(dices), 4)
 
     @staticmethod
     def three(dices, x):
-        return lb(len(dices), 3) + Op.four(dices, x)
+        return lb(len(dices), 3)
 
     @staticmethod
     def pair(dices, x):
-        return lb(len(dices), 2) + Op.three(dices, x)
+        return lb(len(dices), 2)
 
 
 def lb(n, k):
+    if k > n:
+        return 0
     proba_ok = pow(1.0 / 6.0, k)
     proba_ko = pow(5.0 / 6.0, n - k)
     comb = math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
-    return proba_ok * proba_ko * comb * 100
+    return proba_ok * proba_ko * comb * 100 + lb(n, k + 1)
